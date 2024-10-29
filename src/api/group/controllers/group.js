7afd3ad784ce7abab152951ed0f463b1aa0c34e9
igor Sharin <sharinigor1@gmail.com>
2024-10-30 05:23:28 +0900

    test controllers

--- src/api/group/controllers/group.js
+++ src/api/group/controllers/group.js
@@ -1,9 +1,16 @@
-'use strict';
+"use strict";
 
-/**
- * group controller
- */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::group.group');
+module.exports = createCoreController("api::group.group", ({ strapi }) => ({
+  async customMethod(ctx) {
+    try {
+      const { id } = ctx.params; // Получаем id из параметров
+      // Выполняем логику получения данных
+      const data = await strapi.entityService.findOne("api::group.group", id);
+      ctx.send(data);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+}));
