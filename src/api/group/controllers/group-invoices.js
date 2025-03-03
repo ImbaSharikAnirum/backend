0c91b954b3a86480123a69fec3364c0b0e555500
igor Sharin <sharinigor1@gmail.com>
2025-03-03 23:07:56 +0900

    fix

--- src/api/group/controllers/group-invoices.js
+++ src/api/group/controllers/group-invoices.js
@@ -2,8 +2,46 @@
 
 module.exports = {
   async generateInvoice(ctx) {
-    console.log("Привет");
-    console.log("Данные из body:", ctx.request.body);
-    ctx.send({ message: "Запрос обработан" });
+    const { courseId, month, nextMonth } = ctx.request.body;
+
+    try {
+      // Запрос к API Strapi для получения счетов
+      const invoices = await strapi.entityService.findMany(
+        "api::invoice.invoice",
+        {
+          filters: {
+            group: courseId,
+            start_day: { $lte: month?.endOfMonth }, // start_day <= endOfMonth
+            end_day: { $gte: month?.startOfMonth }, // end_day >= startOfMonth
+          },
+        }
+      );
+
+      const copiedInvoices = invoices.map(
+        ({ name, family, phone, currency }) => ({
+          name,
+          family,
+          phone,
+          currency,
+          group: courseId,
+          status_payment: false,
+          start_day: nextMonth?.startDayOfMonth,
+          end_day: nextMonth?.endDayOfMonth,
+          sum: nextMonth?.sum,
+          publishedAt: new Date(),
+        })
+      );
+
+      const createdInvoices = await Promise.all(
+        copiedInvoices.map((invoice) =>
+          strapi.entityService.create("api::invoice.invoice", { data: invoice })
+        )
+      );
+
+      ctx.send({ message: "Копии инвойсов созданы", copiedInvoices });
+    } catch (error) {
+      console.error("Ошибка при получении инвойсов:", error);
+      ctx.send({ error: "Ошибка при обработке запроса" }, 500);
+    }
   },
 };
--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -20,4 +20,4 @@ module.exports = (plugin) => {
   };
 
   return plugin;
-};
+};  
